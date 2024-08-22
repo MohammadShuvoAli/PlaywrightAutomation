6a681ef6adeba4bd0c81af227aced1c34244a53e
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-22 22:41:36 +0600

    update

---
+++ tests/FirstTest.spec.js
@@ -0,0 +1,15 @@
+const {test, expect} = require('@playwright/test')
+
+test('Homepage', async({page})=> {
+    await page.goto("https://www.demoblaze.com/index.html")
+    
+    const pageTitle = await page.title()
+    console.log("Page Title:", pageTitle)
+    await expect(page).toHaveTitle("STORE")
+
+    const pageURL = await page.url()
+    console.log("Page URL:", pageURL)
+    await expect(page).toHaveURL("https://www.demoblaze.com/index.html")
+
+    await page.close()   
+})
