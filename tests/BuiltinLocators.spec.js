212b5688131eb7001af0d9b39410dd8e69a186b2
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-24 00:18:15 +0600

    update

--- tests/BuiltinLocators.spec.js
+++ tests/BuiltinLocators.spec.js
@@ -1,12 +1,11 @@
 const {test, expect} = require('@playwright/test')
 
 test("Built-in Locators", async({page})=>{
-    await page.goto("https://google.com/")
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
-    const logo = await page.getByAltText('Your Store')
+    const logo = await page.getByAltText('company-branding')
 
     await expect(logo).toBeVisible()
 
     await page.close()
-
 })
