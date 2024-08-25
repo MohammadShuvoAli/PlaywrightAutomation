c88f14f7c3e135fbd0ca189679f532ea083dacfa
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-25 23:59:51 +0600

    update

--- tests/SoftAssertions.spec.js
+++ tests/SoftAssertions.spec.js
@@ -1,5 +1,12 @@
 const {test, expect} = require('@playwright/test')
 
 test('Soft Assertions', async({page})=>{
+
+    // Hard Assertions
+    await page.goto('https://www.demoblaze.com')
+    await expect(page).toHaveTitle('STORE')
+    await expect(page).toHaveURL('https://www.demoblaze.com')
+    await expect(page.locator('.navbar-brand')).toBeVisible()
     
+    page.close()
 })
