054ca7a97352a45c2be2f371df54e4666d11835c
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-25 13:44:47 +0600

    update

--- tests/BuiltinLocators.spec.js
+++ tests/BuiltinLocators.spec.js
@@ -1,11 +1,25 @@
+/*
+page.getByText() to locate by text content.
+page.getByLabel() to locate a form control by associated label's text.
+page.getByTitle() to locate an element by its title attribute.
+page.getByTestId() to locate an element based on its data-testid attribute (other attributes can be configured).
+*/
+
 const {test, expect} = require('@playwright/test')
 
 test("Built-in Locators", async({page})=>{
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
+    //page.getByAltText() to locate an element, usually image, by its text alternative.
     const logo = await page.getByAltText('company-branding')
-
     await expect(logo).toBeVisible()
+
+    //page.getByPlaceholder() to locate an input by placeholder.
+    await page.getByPlaceholder("Username").fill("Admin")
+    await page.getByPlaceholder("Password").fill("admin123")
+
+    //page.getByRole() to locate by explicit and implicit accessibility attributes.
+    await page.getByRole('button', {type:'submit'}).click()
 
     await page.close()
 })
