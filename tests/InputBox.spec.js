c7718f200586d78239bd66dce048f3b2f15b0ae6
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-27 01:32:30 +0600

    update

--- tests/InputBox.spec.js
+++ tests/InputBox.spec.js
@@ -1,5 +1,17 @@
 const {test, expect} = require('@playwright/test')
 
 test('Handle Inputbox', async({page})=>{
-    
+    await page.goto("https://testautomationpractice.blogspot.com/")
+
+    await expect(page.locator("#name")).toBeVisible()
+    await expect(page.locator("#name")).toBeEmpty()
+    await expect(page.locator("#name")).toBeEditable()
+    await expect(page.locator("#name")).toBeEnabled()
+
+    await page.locator("#name").fill("shuvo")
+    await page.waitForTimeout(3000)
+    await page.locator("#name").clear()
+    await page.waitForTimeout(3000)
+    await page.fill("#name", "SHUVO")
+    await page.waitForTimeout(3000)
 })
