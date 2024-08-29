4000e211a0f47e8a1515956c8714ec3a361637b8
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-29 23:18:31 +0600

    update

--- tests/RadioButtons.spec.js
+++ tests/RadioButtons.spec.js
@@ -3,5 +3,14 @@ const {test, expect} = require('@playwright/test')
 test('Handle Radio Buttons', async({page})=>{
     await page.goto("https://testautomationpractice.blogspot.com/")
 
+    //Radio Button
+    //await page.locator("#male").check()
+    await page.check("#male")
+    await expect(await page.locator("#male")).toBeChecked()
+    await expect(await page.locator("#male").isChecked()).toBeTruthy()
+
+    await expect(await page.locator("#female")).not.toBeChecked()
+    await expect(await page.locator("#female").isChecked()).toBeFalsy()
+
     await page.waitForTimeout(3000)
 })
