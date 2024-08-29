55501066c9143ca02f83ebf1822bdce71ee81fe6
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-29 23:56:29 +0600

    update

--- tests/Checkboxes.spec.js
+++ tests/Checkboxes.spec.js
@@ -9,4 +9,18 @@ test("Handle Checkboxes", async({page})=>{
     await expect(page.locator("#monday")).toBeChecked()
     await expect(page.locator("#monday").isChecked()).toBeTruthy()
 
+    await page.locator("#monday").uncheck()
+
+    // multiple checkboxes
+    const checkboxLocators = ["#monday", "#friday", "#wednesday"]
+
+    for(const locator of checkboxLocators){
+        await page.locator(locator).check()
+    }
+
+    for(const locator of checkboxLocators){
+        if(await page.locator(locator).isChecked()){
+            await page.locator(locator).uncheck()
+        }
+    }
 })
