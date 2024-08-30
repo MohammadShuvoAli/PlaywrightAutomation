8693204916889c3f64feefad3e9d3378b24938cf
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-30 23:40:50 +0600

    update

---
+++ tests/AutoSuggestDropdown.spec.js
@@ -0,0 +1,21 @@
+const {test, expect} = require('@playwright/test')
+
+test("Handle Auto Suggest Dropdown", async({page})=>{
+    await page.goto("https://www.shohoz.com")
+    await page.locator("#fromcity").click()
+    await page.locator("#fromcity").type('Dha', {delay:2000})
+    
+    await page.waitForSelector(".station-filter-dropdown>button")
+
+    const fromCityOptions = await page.$$(".station-filter-dropdown>button")
+
+    for(let option of fromCityOptions){
+        if(await option.isVisible()){
+            const value = await option.textContent()
+            console.log(value)
+        }
+    }
+    
+    await page.waitForTimeout(5000)
+    await page.close()
+})
