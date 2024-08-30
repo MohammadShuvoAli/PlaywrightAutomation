0eae70a1a56deb353f56a1b2a43ff4f132383d08
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-30 22:43:20 +0600

    update

--- tests/BootstrapDropdown.spec.js
+++ tests/BootstrapDropdown.spec.js
@@ -5,8 +5,14 @@ test("Handle Bootstrap Dropdown", async({page})=>{
 
     await page.locator(".multiselect").click()
     
+    // checking number of options present in dropdown
     const option = await page.locator("ul>li label input")
     await expect(option).toHaveCount(11)
+
+    // checking number of options present in dropdown using JS array
+    const options = await page.$$("ul>li label input")
+    console.log("Number of options in dropdown: ", options.length)
+    await expect(options.length).toBe(11)
 
     await page.waitForTimeout(5000)
     await page.close()
