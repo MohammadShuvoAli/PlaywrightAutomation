937e6fdb0adb598897a5430749bac6975a77a5cc
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-30 21:38:05 +0600

    update

--- tests/Dropdown.spec.js
+++ tests/Dropdown.spec.js
@@ -1,7 +1,19 @@
 const {test, expect} = require('@playwright/test')
 
 test("Handle Dropdowns", async({page})=>{
-
     await page.goto("https://testautomationpractice.blogspot.com")
 
+    // diffent ways of selecting dropdown
+    await page.locator("#country").selectOption({label:'Japan'}) // label / visible text
+    await page.waitForTimeout(3000)
+    await page.locator("#country").selectOption('China') // visible text
+    await page.waitForTimeout(3000)
+    await page.locator("#country").selectOption({value: 'uk'}) // using value
+    await page.waitForTimeout(3000)
+    await page.locator("#country").selectOption({index: 4}) // using index
+    await page.waitForTimeout(3000)
+    await page.selectOption("#country", 'canada') // using text
+    await page.waitForTimeout(3000)
+
+    await page.close()
 })
