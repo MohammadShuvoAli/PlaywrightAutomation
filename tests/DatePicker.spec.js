2760fbd63c5e8158881b241413654d0957537747
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-09-01 00:27:02 +0600

    update

--- tests/DatePicker.spec.js
+++ tests/DatePicker.spec.js
@@ -8,3 +8,29 @@ test("select date by filling the input field", async({page})=>{
     await page.waitForTimeout(5000)
     await page.close()
 })
+
+test.only("select date from calendar popup", async({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com")
+
+    // date information
+    const year = "2026"
+    const month = "September"
+    const date = "15"
+
+    // click on field for calendar pop up
+    await page.locator("#datepicker").click()
+
+    while(true){
+        const currentMonth = await page.locator('.ui-datepicker-month').textContent()
+        const currentYear = await page.locator('.ui-datepicker-year').textContent()
+
+        if(currentYear == year && currentMonth == month){
+            break
+        }
+
+        await page.locator('[title="Next"]').click() // Next button
+    }
+    
+    await page.waitForTimeout(5000)
+    await page.close()
+})
