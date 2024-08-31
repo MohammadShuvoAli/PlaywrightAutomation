fc2ae81d117feb0ce608378f3a4d08dfc53d22ae
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-31 12:57:21 +0600

    update

--- tests/HiddenDropdown.spec.js
+++ tests/HiddenDropdown.spec.js
@@ -11,15 +11,23 @@ test("Handle Hidden Dropdown", async({page})=>{
     //page.getByText() to locate by text content.
     await expect(page.getByText('PIM')).toBeVisible()
 
+    // click on PIM
     await page.click('ul > li:nth-child(2) > a > span')
+    // click on dropdown
+    await page.click('//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[6]/div/div[2]/div/div/div[2]/i')
+    // waiting for options
+    await page.waitForTimeout(3000)
 
-    await page.click("div:nth-child(6) > div > div:nth-child(2) > div > div > div.oxd-select-text-input")
+    const options = await page.$$('//div[@role="listbox"]//span')
 
-    await page.click('//div[@role="listbox]//span"')
+    for(const option of options){
+        const jobTitle = await option.textContent()
+        console.log(jobTitle)
+        if(jobTitle.includes('QA Engineer')){
+            option.click()
+        }
+    }
 
-
-
-    
     await page.waitForTimeout(5000)
     await page.close()
 })
