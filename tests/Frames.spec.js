8f018abc55ca54b6129e95e0b0fb0f360757e953
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-31 16:41:55 +0600

    update

--- tests/Frames.spec.js
+++ tests/Frames.spec.js
@@ -1,8 +1,15 @@
 const {test, expect} = require('@playwright/test')
 
-test("Handle Frames", async({page})=>{
-    await page.goto("")
+test("Handle Frames using name or URL", async({page})=>{
+    await page.goto("https://ui.vision/demo/webtest/frames/")
 
+    // count total number of frames
+    const allFrames = await page.frames()
+    console.log("Number of frames:", allFrames.length)
+
+    // locating frame using URL
+    const frame_1 = await page.frame({url:'https://ui.vision/demo/webtest/frames/frame_1.html'})
+    frame_1.fill('name="mytext1"','Hello Shuvo!')
     
     await page.waitForTimeout(3000)
     await page.close()
