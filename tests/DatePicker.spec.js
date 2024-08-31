5d9ff7d89563ee1e87d9cdff5b7b11009662667d
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-09-01 00:09:56 +0600

    update

--- tests/DatePicker.spec.js
+++ tests/DatePicker.spec.js
@@ -2,7 +2,9 @@ const {test, expect} = require('@playwright/test')
 
 test("Handle Date Picker", async({page})=>{
     await page.goto("https://testautomationpractice.blogspot.com")
+
+    await page.locator("#datepicker").fill('09/15/2024') // mm/dd/yyyy
     
-    await page.waitForTimeout(3000)
+    await page.waitForTimeout(5000)
     await page.close()
 })
