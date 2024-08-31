99280c3507e2556cde8f3939492a28a3c169cb0d
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-31 13:37:44 +0600

    update

--- tests/Alerts.spec.js
+++ tests/Alerts.spec.js
@@ -35,3 +35,24 @@ test("Confirmation Alert - Alert with OK & Cancel button", async({page})=>{
     await page.waitForTimeout(3000)
     await page.close()
 })
+
+test("Prompt Alert - Alert with Inputbox & Buttons", async({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com")
+
+    // Enabling dialog window handler
+    page.on('dialog',async dialog => {
+        expect(dialog.type()).toContain('prompt')
+        expect(dialog.message()).toContain('Please enter your name:')
+        // verify default inputbox value
+        expect(dialog.defaultValue()).toContain('Harry Potter')
+        await dialog.accept('Shuvo') // accept alert by entering text
+        // await dialog.dismiss() // close by using cancel button
+    })
+
+    // click on alert button
+    await page.click('//*[@id="HTML9"]/div[1]/button[3]')
+    await expect(page.locator('//*[@id="demo"]')).toHaveText('Hello Shuvo! How are you today?')
+    
+    await page.waitForTimeout(3000)
+    await page.close()
+})
