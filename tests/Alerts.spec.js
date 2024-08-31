bff5b9ab5dde64c5a24117105a3fa8a908a7429f
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-31 13:29:19 +0600

    update

--- tests/Alerts.spec.js
+++ tests/Alerts.spec.js
@@ -16,3 +16,22 @@ test("Alert with OK", async({page})=>{
     await page.waitForTimeout(3000)
     await page.close()
 })
+
+test("Confirmation Alert - Alert with OK & Cancel button", async({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com")
+
+    // Enabling dialog window handler
+    page.on('dialog',async dialog => {
+        expect(dialog.type()).toContain('confirm')
+        expect(dialog.message()).toContain('Press a button!')
+        await dialog.accept() // close by using OK button
+        // await dialog.dismiss() // close by using cancel button
+    })
+
+    // click on alert button
+    await page.click('//*[@id="HTML9"]/div[1]/button[2]')
+    await expect(page.locator('//*[@id="demo"]')).toHaveText('You pressed OK!')
+    
+    await page.waitForTimeout(3000)
+    await page.close()
+})
