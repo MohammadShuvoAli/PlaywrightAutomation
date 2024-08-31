4544ced8bd0852adca7814246561eb7ec4782882
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-31 16:46:24 +0600

    update

--- tests/Frames.spec.js
+++ tests/Frames.spec.js
@@ -7,9 +7,12 @@ test("Handle Frames using name or URL", async({page})=>{
     const allFrames = await page.frames()
     console.log("Number of frames:", allFrames.length)
 
+    // locating frame using name
+    // const frame_1 = await page.frame('FRAME_NAME')
+
     // locating frame using URL
     const frame_1 = await page.frame({url:'https://ui.vision/demo/webtest/frames/frame_1.html'})
-    frame_1.fill('name="mytext1"','Hello Shuvo!')
+    await frame_1.fill("input[name='mytext1']", 'Hello, Shuvo!')
     
     await page.waitForTimeout(3000)
     await page.close()
