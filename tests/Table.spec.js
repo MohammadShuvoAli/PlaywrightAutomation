f010ea354ff23ca5475677b6978f19441fe243f3
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-08-31 23:12:08 +0600

    update

--- tests/Table.spec.js
+++ tests/Table.spec.js
@@ -35,3 +35,27 @@ test("select specific checkbox from table cell", async({page})=>{
     await page.waitForTimeout(3000)
     await page.close()
 })
+
+test("select multiple checkbox from table cell using function", async({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com")
+
+    const table = await page.locator("#productTable")
+    const columns = await table.locator("thead > tr > th")
+    const rows = await table.locator("tbody > tr")
+
+    // call the function
+    await selectProduct(rows, page, 'Product 1')
+    await selectProduct(rows, page, 'Product 3')
+    await selectProduct(rows, page, 'Product 5')
+    
+    await page.waitForTimeout(3000)
+    //await page.close()
+})
+
+async function selectProduct(rows, page, name){
+    const matchedRow = await rows.filter({
+        has: page.locator('td'),
+        hasText: name
+    })
+    await matchedRow.locator('input').check()
+}
