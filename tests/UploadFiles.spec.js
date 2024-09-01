8a357c1d368ef399229994bfdc44b989827d8f96
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-09-01 17:37:39 +0600

    update

--- tests/UploadFiles.spec.js
+++ tests/UploadFiles.spec.js
@@ -2,8 +2,17 @@ const {test, expect} = require('@playwright/test')
 
 test("Upload Single File", async({page})=>{
     await page.goto("https://the-internet.herokuapp.com/upload")
+    
+    // wait for the selector
+    await page.waitForSelector("#file-upload")
+    // select file for upload
+    await page.locator("#file-upload").setInputFiles('files/test1.pdf')
+    // press upload button
+    await page.locator("#file-submit").click()
 
-    await page.locator("#file-upload").setInputFiles
+    // verify file upload
+    await expect(page.locator("div[class='example'] h3")).toHaveText("File Uploaded!")
+    await expect(page.locator("#uploaded-files")).toHaveText("test1.pdf")
 
     await page.waitForTimeout(3000)
     await page.close()
