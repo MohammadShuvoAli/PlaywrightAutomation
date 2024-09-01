9df0729f404c3ec3d9cc46b85704170dbb0c588e
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-09-01 17:23:49 +0600

    update

--- tests/UploadFiles.spec.js
+++ tests/UploadFiles.spec.js
@@ -3,6 +3,8 @@ const {test, expect} = require('@playwright/test')
 test("Upload Single File", async({page})=>{
     await page.goto("https://the-internet.herokuapp.com/upload")
 
+    await page.locator("#file-upload").setInputFiles
+
     await page.waitForTimeout(3000)
     await page.close()
 })
