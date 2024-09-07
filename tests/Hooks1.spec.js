5e3006d987e5d46e841daab6c5cec95b6c59d5a9
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-09-08 00:34:11 +0600

    update

--- tests/Hooks1.spec.js
+++ tests/Hooks1.spec.js
@@ -1,3 +1,6 @@
+// beforeEach: This hook is executed before each individual test
+// afterEach: This hook is executed after each individual test
+
 const {test, expect} = require('@playwright/test')
 
 let page;
@@ -6,13 +9,11 @@ test.beforeEach(async ({browser})=>{
     page = await browser.newPage() // referring to each test page
 
     await page.goto("https://www.demoblaze.com/index.html")
-
     // Login
     await page.locator("#login2").click()
     await page.locator("#loginusername").fill('shuvo')
     await page.locator("#loginpassword").fill('shuvo123')
     await page.locator("button[onclick='logIn()']").click()
-
 })
 
 test.afterEach(async ()=>{
@@ -30,7 +31,6 @@ test("Homepage Test", async()=>{
 test("Add product to cart", async()=>{
     // Select a product
     await page.locator(".hrefch[href='prod.html?idp_=1']").click()
-
     // Add product to cart
     await page.locator('#tbodyid > div.row > div > a').click()
 
