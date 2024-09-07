b9c99afafdcac874ba940d0e56cd5f5d61a2d3c3
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-09-08 00:20:05 +0600

    update

--- tests/Hooks.spec.js
+++ tests/Hooks.spec.js
@@ -1,6 +1,11 @@
 const {test, expect} = require('@playwright/test')
+const { browser } = require('har-validator')
 
-test("Homepage Test", async({page})=>{
+let page;
+
+test.beforeEach(async ({browser})=>{
+    page = await browser.newPage() // referring to each test page
+
     await page.goto("https://www.demoblaze.com/index.html")
 
     // Login
@@ -9,13 +14,34 @@ test("Homepage Test", async({page})=>{
     await page.locator("#loginpassword").fill('shuvo123')
     await page.locator("button[onclick='logIn()']").click()
 
+})
+
+test.afterEach(async ()=>{
+    // Logout
+    await page.locator('#logout2').click()
+})
+
+test("Homepage Test", async({page})=>{
     // Count total products from homepage    
     await page.waitForSelector('.hrefch')
     const products = await page.$$('.hrefch')
     expect(products).toHaveLength(9)
 
-    // Logout
-    await page.locator('#logout2').click()
+    await page.waitForTimeout(3000)
+    await page.close()
+})
+
+test("Add product to cart", async({page})=>{
+    // Select a product
+    await page.locator(".hrefch[href='prod.html?idp_=1']").click()
+
+    // Add product to cart
+    await page.locator('#tbodyid > div.row > div > a').click()
+
+    page.on('dialog', async dialog => {
+        expect(dialog.message()).toContain('Product added.')
+        dialog.accept()
+    })
 
     await page.waitForTimeout(3000)
     await page.close()
