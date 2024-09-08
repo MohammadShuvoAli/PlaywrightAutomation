0c4d63b2afe7ef85d8812068830a1678e621a0c6
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-09-08 14:38:37 +0600

    update

--- tests/Screenshot.spec.js
+++ tests/Screenshot.spec.js
@@ -1,15 +1,18 @@
-const { test } = require('@playwright/test')
+const { test } = require('@playwright/test');
 
 let page;
 
 test.beforeAll(async({browser})=>{
     page = await browser.newPage()
-
     await page.goto("https://bangladesh.gov.bd/index.php")
 })
 
 test('Page Screenshot', async()=>{
+    // old screenshot will be replaced with new
+    await page.screenshot({path: 'tests/Screenshot/' + 'Homepage.png'}) 
 
+    // Adding date stamp for creating unique screenshot name everytime
+    await page.screenshot({path: 'tests/Screenshot/' + Date.now() + ' Homepage.png'}) 
 })
 
 test('Full Page Screenshot', async()=>{
