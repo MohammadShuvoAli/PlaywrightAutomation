31e8237feb4be24a1f07e09d2032b79b77224663
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-09-08 14:52:52 +0600

    update

--- tests/Screenshot.spec.js
+++ tests/Screenshot.spec.js
@@ -16,9 +16,17 @@ test('Page Screenshot', async()=>{
 })
 
 test('Full Page Screenshot', async()=>{
+    // old screenshot will be replaced with new
+    await page.screenshot({path: 'tests/Screenshot/' + 'FullPage.png', fullPage:true}) 
 
+    // Adding date stamp for creating unique screenshot name everytime
+    await page.screenshot({path: 'tests/Screenshot/' + Date.now() + ' FullPage.png', fullPage:true}) 
 })
 
 test('Page Element Screenshot', async()=>{
-
+    // old screenshot will be replaced with new
+    await page.locator("//div[5]/div[3]").screenshot({path: 'tests/Screenshot/' + 'ElementScreenshot.png'}) 
+    
+    // Adding date stamp for creating unique screenshot name everytime
+    await page.locator("//div[5]/div[3]").screenshot({path: 'tests/Screenshot/' + Date.now() + ' ElementScreenshot.png'})
 })
