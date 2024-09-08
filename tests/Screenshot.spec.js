91ac7653743be8e6c191de05ac795eb9d95ad174
= <102200630+MohammadShuvoAli@users.noreply.github.com>
2024-09-08 14:16:21 +0600

    update

---
+++ tests/Screenshot.spec.js
@@ -0,0 +1,13 @@
+const { test } = require('@playwright/test')
+
+test('Page Screenshot', async({page})=>{
+
+})
+
+test('Full Page Screenshot', async({page})=>{
+
+})
+
+test('Page Element Screenshot', async({page})=>{
+
+})
